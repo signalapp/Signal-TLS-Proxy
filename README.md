c3ef184c81ca0fdc1003035a1125617c7aacd791
Moxie Marlinspike <moxie+github@signal.org>
2021-01-28 10:34:53 -0800

    Big bang

---
+++ README.md
@@ -0,0 +1,10 @@
+# Signal TLS Proxy
+
+To run a Signal TLS proxy, you will need a host with a domain name that has ports 80 and 443 available.
+
+1. Install docker and docker-compose (`apt update && apt install docker docker-compose`)
+1. Clone this repository
+1. `./init-certificate.sh`
+1. `docker-compose up --detach`
+
+Your proxy is now running! You can share this with the URL `https://signal.tube/#<your_host_name>` 
---
+++ init-certificate.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: docker-compose is not installed.' >&2
+  exit 1
+fi
+
+data_path="./data/certbot"
+
+read -p "Enter domain name (eg. www.example.com): " domains
+
+if [ -d "$data_path" ]; then
+  read -p "Existing data found. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
+
+
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+  echo "### Downloading recommended TLS parameters ..."
+  mkdir -p "$data_path/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  echo
+fi
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+docker-compose run -p 80:80 --rm --entrypoint "\
+  sh -c \"certbot certonly --standalone \
+    --register-unsafely-without-email \
+    $domain_args \
+    --agree-tos \
+    --force-renewal && \
+    ln -fs /etc/letsencrypt/live/$domains/ /etc/letsencrypt/active\"" certbot
+echo
+echo "After running 'docker-compose up --detach' you can share your proxy as: https://signal.tube/#$domains"
